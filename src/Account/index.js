47c0f47d5a243162198b7bd40f0fe07d53b3b4b6
Jacob Mink <jacobmink@gmail.com>
2019-02-04 16:28:40 -0700

    Pre-bonus work complete

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,14 +2,43 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor(){
+    super();
+    this.state = {
+      balance: 0
+    };
+    this.inputHolder = 0;
+  }
+  depositMoney = (e)=>{
+    this.setState({
+      balance: parseInt(this.state.balance) + parseInt(this.inputHolder.value)
+    })
+  }
+  withdrawMoney = ()=>{
+    if(this.state.balance >= this.inputHolder.value){
+      this.setState({
+        balance: parseInt(this.state.balance) - parseInt(this.inputHolder.value)
+      });
+    }
+  }
   render() {
+    let balanceClass = 'balance';
+    if(this.state.balance == 0){
+      balanceClass += ' zero'
+    }
+
+
+
+
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={balanceClass}>${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(inputText)=>
+          this.inputHolder = inputText
+        } />
+        <input type="button" value="Deposit" onClick={this.depositMoney} />
+        <input type="button" value="Withdraw" onClick={this.withdrawMoney} />
       </div>
     )
   }
