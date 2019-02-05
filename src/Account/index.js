5a3ed7c505625a84e8c43140e315bc0f8d8e7adb
Jacob Mink <jacobmink@gmail.com>
2019-02-04 21:53:08 -0700

    restricted input types

--- src/Account/index.js
+++ src/Account/index.js
@@ -5,40 +5,59 @@ class Account extends Component {
   constructor(){
     super();
     this.state = {
-      balance: 0
+      balance: 0,
+      value: 0
     };
     this.inputHolder = 0;
   }
   depositMoney = (e)=>{
-    this.setState({
-      balance: parseInt(this.state.balance) + parseInt(this.inputHolder.value)
-    })
+      this.setState({
+        balance: parseInt(this.state.balance) + parseInt(this.state.value)
+      });
+      // console.log(this.state.value)
   }
   withdrawMoney = ()=>{
-    if(this.state.balance >= this.inputHolder.value){
+    if(this.state.balance >= this.state.value){
       this.setState({
-        balance: parseInt(this.state.balance) - parseInt(this.inputHolder.value)
+        balance: parseInt(this.state.balance) - parseInt(this.state.value)
       });
     }
   }
+  handleSubmit = (e)=>{
+    e.preventDefault();
+  }
+  handleChange = (e)=>{
+    // console.log(typeof e.target.value)
+    if(e.target.value && !isNaN(e.target.value)){
+      this.setState({
+        value: e.target.value
+      });
+      console.log(this.state.value + ' / ' + e.target.value)
+    }else{
+      console.log(this.state.value + ' / ' + e.target.value)
+      this.setState({
+        value: 0
+      })
+    }
+  } 
   render() {
     let balanceClass = 'balance';
-    if(this.state.balance == 0){
+    if(this.state.balance === 0){
       balanceClass += ' zero'
     }
-
-
-
-
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className={balanceClass}>${this.state.balance}</div>
-        <input type="text" placeholder="enter an amount" ref={(inputText)=>
-          this.inputHolder = inputText
-        } />
-        <input type="button" value="Deposit" onClick={this.depositMoney} />
-        <input type="button" value="Withdraw" onClick={this.withdrawMoney} />
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="enter an amount" onChange={this.handleChange} />
+          <input type="submit" value="Deposit" onClick={this.depositMoney} />
+          <input type="submit" value="Withdraw" onClick={this.withdrawMoney} />
+
+
+        </form>
+        
       </div>
     )
   }
